115a6aae58ab276246a84cc27460e1aac338f67e
hamioo <zhouhaimi@jiangduoduo.com>
2018-11-01 16:42:28 +0800

    django列表

--- zhmProject/views.py
+++ zhmProject/views.py
@@ -2,36 +2,80 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
-from  django.contrib.auth.decorators import login_required
-from zhmProject.models import Event
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from zhmProject.models import Event, Guest
+
+
 # Create your views here.
 def index(request):
-    #return HttpResponse("Hello Django!")
-    return render(request,"index.html")
+    # return HttpResponse("Hello Django!")
+    return render(request, "index.html")
+
+
 def login_action(request):
     if request.method == 'POST':
-        username = request.POST.get('username','')
-        password = request.POST.get('password','')
-        user = auth.authenticate(username=username,password=password)
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
         if user is not None:
-            auth.login(request,user)
-        #if username == 'admin' and password =='admin':
-           # return HttpResponse(u'登录成功')
-           # return HttpResponseRedirect('/event_manage/')
-           # response = HttpResponseRedirect('/event_manage/')   # 添加浏览器cookie
-           # response.set_cookie('user',username,3600)
-           # return response
+            auth.login(request, user)
+            # if username == 'admin' and password =='admin':
+            # return HttpResponse(u'登录成功')
+            # return HttpResponseRedirect('/event_manage/')
+            # response = HttpResponseRedirect('/event_manage/')   # 添加浏览器cookie
+            # response.set_cookie('user',username,3600)
+            # return response
             request.session['user'] = username
             response = HttpResponseRedirect('/event_manage/')
             return response
         else:
-            return render(request,'index.html',{'error':u'用户名或密码错误！'})
+            return render(request, 'index.html', {'error': u'用户名或密码错误！'})
+
+
 @login_required
 def event_manage(request):
     # username = request.COOKIES.get('user','') #读取浏览器cookie
     event_list = Event.objects.all()
-    username = request.session.get('user','')
+    username = request.session.get('user', '')
 
-    return render(request,"event_manage.html",{"user":username,"events":event_list})
+    return render(request, "event_manage.html", {"user": username, "events": event_list})
+
+
+@login_required
+def search_name(request):
+    username = request.session.get('user', '')
+    search_name = request.GET.get("name", "")
+    event_list = Event.objects.filter(name__contains=search_name)
+    return render(request, "event_manage.html", {"user": username, "events": event_list})
+
+
+@login_required
+def guest_manage(request):
+    username = request.session.get('user', '')
+    guest_list = Guest.objects.all()
+    paginator = Paginator(guest_list,2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        contacts = paginator.page(1)
+    except EmptyPage:
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, "guest_manage.html", {"user": username, "guests": contacts})
+
+
+@login_required
+def search_phone(request):
+    username = request.seesion.get('user', '')
+    search_phone = request.GET.get("phone", "")
+    guest_list = Guest.objects.filter(phone__contains=search_phone)
+    return render(request, "guest_manage.html", {"user": username, "guests": guest_list})
+# 退出登录
+@login_required
+def logout(request):
+    auth.logout(request)  # 退出登录
+    response = HttpResponseRedirect('/index/')
+    return response
--- zhmTest/urls.py
+++ zhmTest/urls.py
@@ -21,5 +21,9 @@ urlpatterns = [
     url(r'^index/',views.index),
     url(r"^login_action/$",views.login_action),
     url(r'^event_manage/$',views.event_manage),
-    url(r'^accounts/login/$',views.index)
+    url(r'^accounts/login/$',views.index),
+    url(r"^search_name/$",views.search_name),
+    url(r"^guest_manage/$",views.guest_manage),
+    url(r"^search_phone/$",views.search_name),
+    url(r"^logout/$",views.logout),
 ]
